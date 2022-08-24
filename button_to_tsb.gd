5f9078da7ded6bc138ca367a4f54346bb2f785b3
Fux <109365408+Fuxfantx@users.noreply.github.com>
2022-08-25 01:08:42 +0800

    Implementing Optimization for Buttons with Corner Radius

--- button_to_tsb.gd
+++ button_to_tsb.gd
@@ -1,41 +1,56 @@
+# CC0 Licensed.
+
 extends TouchScreenButton
-class_name ButtonToTSB
+class_name ButtonToTSB  ## Turn Your Button into TouchScreenButton!
 var parent:Button
 var button_event:InputEventMouseButton
+@export var optimized_for_round_button:bool = false
 
 func p():
-	if parent is Button:
-		if parent.disabled: return
+	if parent.disabled: return
 	button_event.pressed = true
 	button_event.position = get_parent().get_screen_position()
+	if optimized_for_round_button:
+		button_event.position.x += get_parent().get_global_rect().size.x / 2
+		button_event.position.y += get_parent().get_global_rect().size.y / 2
 	get_viewport().push_input(button_event)
 func r():
-	if parent is Button:
-		if parent.disabled: return
+	if parent.disabled: return
 	button_event.pressed = false
 	button_event.position = get_parent().get_screen_position()
+	if optimized_for_round_button:
+		button_event.position.x += get_parent().get_global_rect().size.x / 2
+		button_event.position.y += get_parent().get_global_rect().size.y / 2
 	get_viewport().push_input(button_event)
+func u():
+	var rect:RectangleShape2D = RectangleShape2D.new()
+	rect.size = parent.size
+	shape = rect
+	position.x = parent.size.x / 2
+	position.y = parent.size.y / 2
+
 
 func _enter_tree():
-	# 防止Bug
+	# Bug Prevent
 	parent = get_parent() as Button
-	if not (parent is Button): queue_free()
+	if parent==null: queue_free()
 	if OS.has_feature("pc"): queue_free()
 
-	# 鼠标模拟
+	# Emulate the Mouse
 	button_event = InputEventMouseButton.new()
 	button_event.button_index = 1
 
-	# 更新按钮大小
+	# Update Button's Size
 	var rect:RectangleShape2D = RectangleShape2D.new()
 	rect.size = parent.size
 	shape = rect
 
-	# 更新按钮位置
+	# Update Button's Position
 	position.x = parent.size.x / 2
 	position.y = parent.size.y / 2
 
-	# 基础设置
+	# Configurations
 	visibility_mode = TouchScreenButton.VISIBILITY_TOUCHSCREEN_ONLY
 	pressed.connect(p)
 	released.connect(r)
+	parent.resized.connect(u)
